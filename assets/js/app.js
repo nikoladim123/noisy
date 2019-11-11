a63014c9136a350565e6eb38eb7b43ba9811f1be
unknown <nikoladimitrijevic1991@gmail.com>
2019-11-11 19:08:26 +0100

    footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,48 @@ var servicesImg = document.getElementsByClassName('servicesImg');
 
 // services
 function servicesBrightnesFun() {
-  if (servicesAnimTopContainer[0].getClientRects()[0].top < -window.innerHeight/100 *1) {
+  if (servicesAnimTopContainer[0].getClientRects()[0].top < -window.innerHeight / 100 * 1) {
     servicescirc[0].style.filter = 'brightness(1)';
     servicesImg[0].style.filter = 'brightness(1)';
-  }else{
+  } else {
     servicescirc[0].style.filter = 'brightness(0)';
     servicesImg[0].style.filter = 'brightness(0)';
   }
 }
 
-window.addEventListener('scroll',()=>{
+// carousel three
+var carouselThree = document.getElementById('carouselThree');
+var carThreeLeftArrow = document.getElementById('carThreeLeftArrow');
+var carThreeRightArrow = document.getElementById('carThreeRightArrow');
+carThreeLeftArrow.addEventListener('click', function() {
+  leftArrowThree();
+  console.log('do i work?');
+});
+carThreeRightArrow.addEventListener('click', function() {
+  rightArrowThree();
+  console.log('do i work?');
+});
+var moduloThree = 1002;
+
+function leftArrowThree() {
+  moduloThree--;
+  carMoveThree();
+}
+
+function rightArrowThree() {
+  moduloThree++;
+  carMoveThree();
+}
+
+function carMoveThree() {
+  if (moduloThree % 2 === 0) {
+    carouselThree.style.left = '0%'
+  }
+  if (moduloThree % 2 === 1) {
+    carouselThree.style.left = '-50%'
+  }
+}
+
+window.addEventListener('scroll', () => {
   servicesBrightnesFun();
-})
+});
